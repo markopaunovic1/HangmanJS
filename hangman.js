358e23d6683af34c03c09c29c5c189b5e639422c
markopaunovic1 <marpau970514@hotmail.com>
2023-10-22 23:46:02 +0200

    first commit

---
+++ hangman.js
@@ -0,0 +1,11 @@
+/* Implementera en variant av hangman.
+
+Lägg till stöd för att ett ord automatiskt via ett HTTP anropp. https://random-word-api.herokuapp.com/word
+
+Koden versionshanteras via ett git repo. Gärna i en egen branch där en pull request är skapad
+
+Inget fulhack utan snygg strukturerad kod som skulle kunna "produktionssättas" */
+
+const Hangman = () => {
+    
+}
